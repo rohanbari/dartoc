4e2e6e56c907416cee81d3c03b0c52b979c0b618
rohanbari <rohanbari4@gmail.com>
2021-12-23 23:06:53 +0530

    Evaluating average using stdin.readLineSync
      * Learned how to use stdin.readLineSync in functions.
      * Explored ~/ (truncating division) operator

--- bin/dartoc.dart
+++ bin/dartoc.dart
@@ -1,3 +1,24 @@
-void main(List<String> arguments) {
-  print('Hello world from Dartoc!');
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dartoc/math.dart';
+
+/// Obtain the user input as an integer.
+int getUserInput() {
+  String? str = stdin.readLineSync(encoding: utf8);
+  int value = 0;
+
+  if (str != null) value = int.parse(str);
+  return value;
+}
+
+void main(List<String> args) {
+  print('Enter two values separated by a newline below.');
+
+  int var1 = getUserInput();
+  int var2 = getUserInput();
+
+  int average = add(var1, var2) ~/ 2;
+
+  print('The average of $var1 and $var2 is $average.');
 }
